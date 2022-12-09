764738a5ce3599cd6eeb929e4458c56a21f55d5e
Nachiketha <98515406+Nachiketha237@users.noreply.github.com>
2022-12-09 17:08:35 +0530

    Update dabba.c

--- dabba.c
+++ dabba.c
@@ -1,25 +1,33 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
 int* canSeePersonsCount(int* heights, int heightsSize, int* returnSize){
 int i,j,k,count=0;
+int maxh;
 int* ar=(int*)malloc(sizeof(int)*heightsSize);
-int maxh=heights[1];
  *returnSize=heightsSize;
 for(i=0;i<heightsSize;i++)
- {
+ {   
+     if(i!=heightsSize-1)
+       maxh=heights[i+1];
      for(j=i+1;j<heightsSize;j++)
      {
+        
         if(heights[i]>heights[j])
         {
-            if(maxh<heights[j])
+            if(maxh<=heights[j])
             {
                 maxh=heights[j];
                 count++;
             }
-            else
-             count++;
+            else{
+            continue;
+            }
         }
         else
         {
         count++;
+        maxh=heights[i+1];
         break;
         }
          
